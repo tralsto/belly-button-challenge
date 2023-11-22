9a7cff73f2f6930e4bf347bd9bf056d5a80f5f01
Tait Ralston <taitralston@gmail.com>
2023-11-22 11:07:00 -0800

    had to fix code to populate the chart, bar and bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -5,57 +5,116 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 const dataPromise = d3.json(url);
 console.log("Data Promise: ", dataPromise);
 
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-  console.log("data", data);
-});
-
-// Create dropdown menu
-let dropdownMenu = d3.select("#selDataset");
-
 // Function to initialize page
 function init() {
 
+  // Fetch the JSON data and console log it
   d3.json(url).then(function(data) {
+    console.log("data", data);
+  
 
-  })
+    // Create dropdown menu with options
+    let dropdownMenu = d3.select("#selDataset");
+    data.names.forEach(name => {
+      dropdownMenu.append("option").text(name).property("value", name);
+    });
 
-
-}
-// #2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-function {
-  var data = [{
-    type: 'bar',
-    x: [20, 14, 23],
-    y: ['giraffes', 'orangutans', 'monkeys'],
-    orientation: 'h'
-  }];
-
-  Plotly.newPlot('bar', data);
-}
-
-// #3. Create a bubble chart that displays each sample.
-function bubbleChart(dataSelection) {
-  var trace1 = {
-    x: ,
-    y: ,
-    mode: 'markers',
-    marker: {
-      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-      opacity: [1, 0.8, 0.6, 0.4],
-      size: [40, 60, 80, 100]
-    }
-  };
-          
-  var data = [trace1];
-    var layout = {
-      title: 'Marker Size and Color',
-      showlegend: false,
-      height: 600,
-      width: 600
-    };
-      
-    Plotly.newPlot('bubble', data, layout);
+    // Initialize the default bar chart with the first data point
+    optionChanged(data.names[0]);
+  });
 };
 
+// #2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+function hbarChart(sample){
+
+  // Fetch the JSON data
+  d3.json(url).then(function(data) {
+    
+    // Gather data for hbar chart from dataset
+    let selectedData = data.samples.filter(s => s.id === sample) [0];
+    
+    // Create data for hbar chart
+    let barData = [{
+      type: 'bar',
+      x: selectedData.sample_values.slice(0, 10).reverse(),
+      y: selectedData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
+      text: selectedData.otu_labels.slice(0, 10).reverse(),
+      orientation: 'h'
+    }];
+
+    // Create layout for bar chart
+    let barLayout = {
+      xaxis: {title: "Sample Values"},
+      yaxis: {title: "OTU ID"}
+    };
+
+    // Plot the horizontal bar chart
+    Plotly.newPlot('bar', barData, barLayout);
+  });
+};
+
+// #3. Create a bubble chart that displays each sample.
+function bubbleChart(sample) {
+
+  // Fetch the JSON data
+  d3.json(url).then(function(data) {
+
+    // Gather data for bubble chart from dataset
+    let selectedData = data.samples.filter(s => s.id === sample) [0];
+
+    // Create data for bubble chart
+    let bubbleData = [{
+      x: selectedData.otu_ids,
+      y: selectedData.sample_values,
+      mode: 'markers',
+      marker: {
+        color: selectedData.otu_ids,
+        opacity: 0.8,
+        size: selectedData.sample_values
+      },
+      text: selectedData.otu_labels
+    }];
+        
+    // Create layout for bubble chart
+    let bubbleLayout = {
+      xaxis: { title: "OTU ID" },
+      yaxis: { title: "Sample Values" }
+    };
+
+    // Plot the bubble chart  
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+
+  });
+};
+
+// #4. Display the sample metadata, i.e., an individual's demographic information.
+function optionChanged(sample) {
+
+  // Update metadata from dataset
+  updateMetadata(sample);
+
+  // Update hbar chart with metadata
+  hbarChart(sample);
+
+  // Update bubble chart with metadata
+  bubbleChart(sample);
+}
+
+// Create function to fetch updated metadata samples
+function updateMetadata(sample) {
+  d3.json(url).then(function(data) {
+    let metaData = data.metadata.filter(m => m.id == sample)[0];
+    let metadataPanel = d3.select("#sample-metadata");
+
+    // Clear existing data if any before adding new data
+    metadataPanel.html("");
+
+    // Append new metadata to demographic info table
+    Object.entries(metaData).forEach(([key, value]) => {
+      metadataPanel.append("p").text(`${key}: ${value}`);
+    });
+});
+}
+
+// Call init function from beginning of code to intialize page load
 init(); 
